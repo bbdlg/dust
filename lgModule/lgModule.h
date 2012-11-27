7148e034fa63a4242aa93ef5279a4cb359156a72
sarry98 <shiluo.110@163.com>
2012-11-27 11:16:59 +0800

    add GNU info for *.sh,*.c, and *.h

--- lgModule/lgModule.h
+++ lgModule/lgModule.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the DFC.
+ *
+ * Copyright (C) 2012 by bbdlg <shiluo.110@163.com>
+ *
+ * The DFC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The DFC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DFC. If not, see <http://www.gnu.org/licenses/>.
+ * */
+
 #ifndef _LGMODULE_H_
 #define _LGMODULE_H_
 /****************************************************************************
