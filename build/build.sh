6cc65efc2c340ffd942475a53afe0497509ba844
sarry98 <shiluo.110@163.com>
2013-01-06 12:59:42 +0800

    remove all module CMakeLists.txt and update build.sh

---
+++ build/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+exec_target=dfc
+
+function help()
+{
+   echo "Usage: $0 [help|dust|farm|clean|distclean]"
+   echo "   help,       show this information;"
+   echo "   dust,       generate executable file: ${exec_target};"
+   echo "   farm,       generate FARM develop platform, you may add a version after 'farm', eg: $0 farm 1.0.0;"
+   echo "   clean,      just clean current project temp files;"
+   echo "   distclean,  clean all make/cmake files;"
+   echo "   without any parameter will be equal to <$0 dust>."
+   echo
+}
+
+if [ "$1" = "farm" ]; then
+   cmake -Wno-dev -Ddust2farm:bool=on --build .. && make
+   mv bin/lib${exec_target}.a .
+   ../platform/dust2farm.sh $1 $2
+   #add other option
+elif [ "$1" = "clean" ]; then
+   make clean
+elif [ "$1" = "distclean" ]; then
+   ls | grep -v `basename $0` | xargs rm -rf 
+elif [ "$1" = "dust" -o "$1" = "" ]; then
+   cmake -Wno-dev -Ddust2farm:bool=off --build .. && make
+   mv bin/${exec_target} .
+else
+   help
+fi
+
--- platform/dust2farm.sh
+++ platform/dust2farm.sh
@@ -19,43 +19,12 @@
 
 ### this script should be in "dust/platform/"
 
-IS_EXIT=YES
-case $1 in
-   help)
-      echo "Usage: `basename $0` [platform_name version]"
-      echo "       `basename $0` [help | clear | make | reset | test]"
-      echo "       help,  show this info"
-      echo "       clear, clear compiling temp files in dust/build/"
-      echo "       make,  just make, if any error, try \"`basename $0` reset\""
-      echo "       test,  generate platform without package"
-      echo
-      ;;
-   clear)
-      ### clear compile temp files, such as Makefile, *.o, etc.
-      cd ../build/ && ls -a |grep -v configure.in |grep -v Makefile.am |grep -v "^\.$" | grep -v "^\.\.$"|xargs rm -rf && cd -
-      ;;
-   make)
-      cd ../build/ && make && cd -
-      ;;
-   reset)
-      cd ../build/ && aclocal && autoconf && autoheader && automake --add-missing && ./configure && make clean && make && cd -
-      ;;
-   *)
-      IS_EXIT=NO
-esac
-if [ ${IS_EXIT} = YES ]; then
-   exit 1
-fi
-
-
 ### package develop platform named "farm":)
 FARM="farm"
 VER="1.0.0"
-TARGETDIR=${FARM}_${VER}
-TARGET=${TARGETDIR}.tar.gz
 
 if [ $# != 2 ]; then
-   echo "Now use default value, target name will be ${TARGET}"
+   echo -n "Now use default value, "
    echo
 else
    FARM=$1
@@ -64,9 +33,7 @@ fi
 
 TARGETDIR=${FARM}_${VER}
 TARGET=${TARGETDIR}.tar.gz
-
-cd ../build/ && aclocal && autoconf && autoheader && automake --add-missing && ./configure && make clean && make && cd -
-echo
+echo "target name will be ${TARGET}"
 
 if [ -d ${TARGETDIR} ]; then
    #mv ${TARGETDIR} ${TARGETDIR}_`date +%Y-%m-%d_%H_%M_%S`_bak
@@ -82,31 +49,27 @@ mkdir ${TARGETDIR}/conf
 #mkdir ${TARGETDIR}/log 
 #mkdir ${TARGETDIR}/data 
 
-cp ./farm_README                 ${TARGETDIR}/README
-cp ../commModule/commModule.h    ${TARGETDIR}/include
-cp ../errModule/errModule.h      ${TARGETDIR}/include
-cp ../logModule/logModule.h      ${TARGETDIR}/include
-cp ../lgModule/lgModule.h        ${TARGETDIR}/include
-cp ../toolsModule/toolsModule.h  ${TARGETDIR}/include
-cp ./farm_userapp.h              ${TARGETDIR}/include/userapp.h
-cp ./main.h                      ${TARGETDIR}/include
-cp ../logModule/seelog.sh        ${TARGETDIR}/bin
-cp ../lgModule/lp.sh             ${TARGETDIR}/bin
-cp ./farm2crop.sh                ${TARGETDIR}/bin
-cp ./crop_*                      ${TARGETDIR}/bin/
-cp ./farm_userapp.c              ${TARGETDIR}/src/userapp.c
-cp ./farm_Makefile.am            ${TARGETDIR}/src/Makefile.am
-cp ./farm_makefile               ${TARGETDIR}/src/makefile
-cp ./farm_configure.in           ${TARGETDIR}/src/configure.in
-cp ../build/libdust.a            ${TARGETDIR}/lib/
-cp ./farm_dust.conf              ${TARGETDIR}/conf/dust.conf
+cp ../platform/farm_README       ${TARGETDIR}/README
+for header in `find ../module* -type f -name "*.h"`
+do
+   cp ${header} ${TARGETDIR}/include
+done
+cp ../platform/userapp.h         ${TARGETDIR}/include
+cp ../platform/main.h            ${TARGETDIR}/include
+cp ../moduleLog/seelog.sh        ${TARGETDIR}/bin
+cp ../moduleLg/lp.sh             ${TARGETDIR}/bin
+cp ../platform/farm2crop.sh      ${TARGETDIR}/bin
+cp ../platform/crop_*            ${TARGETDIR}/bin
+cp ../platform/userapp.c         ${TARGETDIR}/src/userapp.c
+cp ../build/libdfc.a             ${TARGETDIR}/lib/
+cp ../platform/farm_dust.conf    ${TARGETDIR}/conf/dust.conf
 
 ### package
 if [ "$1" = "test" ]; then
    echo "You can check <${TARGETDIR}> in current directory."
 else
    tar czf ${TARGET} ${TARGETDIR}
-   rm -rf ${TARGETDIR}
+   #rm -rf ${TARGETDIR}
    echo "Target<${TARGET}> will be found in current directory."
 fi
 
